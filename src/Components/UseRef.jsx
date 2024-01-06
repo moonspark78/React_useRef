66f37f23dc7a8a9f3b2a268089ba2d1a35f16710
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-06 17:44:25 +0100

    working progress  :rocket: :tada:

--- src/Components/UseRef.jsx
+++ src/Components/UseRef.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 export const UseRef = () => {
+    const [count, setCount] = useState(0);
+    const countRef = useRef(0);
+
+    const handleIncrement = () => {
+
+        setCount(count+1);
+        countRef.current++;
+
+        console.log("State", count);
+        console.log("Ref", countRef.current);
+    };
+
+
+
+
   return (
-    <div>UseRef</div>
+    <div className='ref'>
+        Count: {count}
+        <button onClick={handleIncrement}>Increment</button>
+    </div>
   )
 }
